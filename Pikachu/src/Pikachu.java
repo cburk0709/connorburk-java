cb31da16b347d57f87b495e89244cf9b670c7a92
fruit-zip <cburk0709@gmail.com>
2018-09-06 08:55:47 -0700

    Added Pikachu ASCII

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,10 @@
+public class Pikachu {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of Pokemon!");
+        //Text
+        System.out.println(" (\\__/)");
+        System.out.println(" (*^.^)");
+        System.out.println("z(_(\")(\")");
+        //Pikachu ASCII
+    }
+}
