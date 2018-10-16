4799bd60f261fbfd0b7586effb48e26ae77fe817
fruit-zip <cburk0709@gmail.com>
2018-10-16 08:59:03 -0700

    made it work

--- PrtintBoxesWithConstants/src/PrintBoxesWithConstants.java
+++ PrtintBoxesWithConstants/src/PrintBoxesWithConstants.java
@@ -1,9 +1,11 @@
 public class PrintBoxesWithConstants {
     public static void main(String[] args) {
 	//The first var is height, second var is width, third var is the char printed
-	drawBox(4, 5, /*FIX*/);
+	drawBox(4, 5, '#');
+	drawBox(6, 8, '$');
+	drawBox(2, 7, '%');
     }
-    public static void drawBox(int x, int y, /*FIX*/ z) {
+    public static void drawBox(int x, int y, char z) {
         for(int i = 1; i <= x; i++) {
             for (int j = 1; j <= y; j++) {
                 System.out.print(z);
