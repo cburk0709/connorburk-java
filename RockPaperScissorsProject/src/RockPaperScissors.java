156fead56ac13382634494c00aa19afaf0b9ef45
fruit-zip <cburk0709@gmail.com>
2018-11-14 10:04:55 -0800

    Put the game back in the main method

--- RockPaperScissorsProject/src/RockPaperScissors.java
+++ RockPaperScissorsProject/src/RockPaperScissors.java
@@ -1,3 +1,9 @@
+/*
+Connor Burk
+AP CS A
+Rock Paper Scissors Project
+11-07-18
+ */
 import java.util.Scanner;
 import java.util.Random;
 public class RockPaperScissors {
@@ -6,16 +12,32 @@ public class RockPaperScissors {
         Random rand = new Random();
         System.out.println("Would you like to play rock paper scissors? (y/n)");
         String gameAnswer = input.next();
-        System.out.println("How many rounds?");
-        int roundCount = input.nextInt();
-        System.out.print("What is your name? - ");
-        String playerName = input.next();
         while(gameAnswer.equals("y")) {
+            System.out.println("How many rounds?");
+            int roundCount = input.nextInt();
+            System.out.print("What is your name? - ");
+            String playerName = input.next();
             for(int i = 1; i <= roundCount; i++){
                 System.out.println("ROUND " + i);
                 int weaponAnswerInt = weaponStringToInt();
                 int computerWeapon = rand.nextInt(3);
-                playGame(computerWeapon, weaponAnswerInt, playerName);
+                if (computerWeapon == weaponAnswerInt) {
+                    System.out.println("It's a tie!\n");
+                } else if (computerWeapon == 0 && weaponAnswerInt == 2) {
+                    System.out.println("The machine chose ROCK, beating " + playerName + "'s SCISSORS\n");
+                } else if (computerWeapon == 0 && weaponAnswerInt == 1) {
+                    System.out.println("The machine chose ROCK, losing to " + playerName + "'s PAPER\n");
+                } else if (computerWeapon == 1 && weaponAnswerInt == 0) {
+                    System.out.println("The machine chose PAPER, beating " + playerName + "'s ROCK\n");
+                } else if (computerWeapon == 1 && weaponAnswerInt == 2) {
+                    System.out.println("The machine chose PAPER, losing to " + playerName + "'s SCISSORS\n");
+                } else if (computerWeapon == 2 && weaponAnswerInt == 1) {
+                    System.out.println("The machine chose SCISSORS, beating " + playerName + "'s PAPER\n");
+                } else if (computerWeapon == 2 && weaponAnswerInt == 0) {
+                    System.out.println("The machine chose SCISSORS, losing to " + playerName + "'s ROCK\n");
+                } else if (weaponAnswerInt == 3){
+                    System.out.println("Please choose a weapon");
+                }
             }
             System.out.println("Would you like to play again? (y/n)");
             gameAnswer = input.next();
@@ -33,25 +55,6 @@ public class RockPaperScissors {
             return 2;
         } else {
             return 3;
-        }
-    }
-    public static void playGame(int computerWeapon1, int weaponAnswerInt1, String playerName1){
-        if (computerWeapon1 == weaponAnswerInt1) {
-            System.out.println("It's a tie!\n");
-        } else if (computerWeapon1 == 0 && weaponAnswerInt1 == 2) {
-            System.out.println("The machine chose ROCK, beating " + playerName1 + "'s SCISSORS\n");
-        } else if (computerWeapon1 == 0 && weaponAnswerInt1 == 1) {
-            System.out.println("The machine chose ROCK, losing to " + playerName1 + "'s PAPER\n");
-        } else if (computerWeapon1 == 1 && weaponAnswerInt1 == 0) {
-            System.out.println("The machine chose PAPER, beating " + playerName1 + "'s ROCK\n");
-        } else if (computerWeapon1 == 1 && weaponAnswerInt1 == 2) {
-            System.out.println("The machine chose PAPER, losing to " + playerName1 + "'s SCISSORS\n");
-        } else if (computerWeapon1 == 2 && weaponAnswerInt1 == 1) {
-            System.out.println("The machine chose SCISSORS, beating " + playerName1 + "'s PAPER\n");
-        } else if (computerWeapon1 == 2 && weaponAnswerInt1 == 0) {
-            System.out.println("The machine chose SCISSORS, losing to " + playerName1 + "'s ROCK\n");
-        } else if (weaponAnswerInt1 == 3){
-            System.out.println("Please choose a weapon");
         }
     }
 }
