0b3b1020517df6e6e4d0df1d5c72588708ddf4ca
fruit-zip <cburk0709@gmail.com>
2018-11-29 09:16:18 -0800

    worked on 4.2

--- ArrayWorksheets/src/WorksheetFourPointTwo.java
+++ ArrayWorksheets/src/WorksheetFourPointTwo.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
 public class WorksheetFourPointTwo {
     public static void main(String[] args) {
         //Exercise 1a.
         //14, 6, 2200, 96
 
         //Exercise 1b.
+        //4, 99, 2, 44, 11
+
+        //Exercise 1c.
+        //3, -18, 6, 3, 9
+
+        //Exercise 1d.
+        int[] arrayOne = {10, -2, 4, -4, 9, -5, 19, -7, 39, -1};
+        int max = arrayOne[0];
+        for (int x : arrayOne){
+            max = Math.max(arrayOne[x], max);
+        }
+        int avg;
+        //Exercise 1e.
+        for (int x : arrayOne){
+            avg = 
+        }
     }
 }
